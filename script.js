eece1754df819bb1b741b1d0c27696582b156004
Raam <raam.sanghani@gmail.com>
2023-10-20 13:11:08 -0400

    included game function which plays 5 rounds, keeps score and then declares a winner (line 56).  Something to add next would be a way for the player to make a different selection each time, rather than only the computer being able to change each round.

--- script.js
+++ script.js
@@ -1,64 +1,49 @@
-// Need:
-// getComputerChoice() function ---> is the computer choosing rock, paper, or scissors? returns either of these three values as a string
-// computerSelection = getComputerChoice()
-// playRound(playerSelection, computerSelection) ---> playerSelection is manually input... for now
-
-
 function getComputerChoice() {
-    // const rpsNumber = Math.floor(Math.random() * 3) + 1;
-    // let selection = "";
-    // if (rpsNumber === 1) {
-    //     selection = "rock";
-    // } 
-    // else if (rpsNumber === 2) {
-    //     selection = "paper";
-    // }
-    // else if (rpsNumber === 3) {
-    //     selection = "scissors";
-    // } 
-    // else {
-    //     console.log("Your code is fucked up");
-    // }
-
-    // return selection;
     const weapon = ["rock", "paper", "scissors"];
     return weapon[Math.floor(Math.random() * 3)];
-
 }
 
 const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+// let computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
+    //This function returns true if the player wins the round, false if not, and simply logs if draw
     let flatPlayerSelection = playerSelection.toLowerCase();
     if (flatPlayerSelection === computerSelection) {
         console.log("draw");
+        return "draw"
     }
 
     else if (flatPlayerSelection === "rock") {
         if (computerSelection === "scissors"){
             console.log("round goes to player");
+            return true
         }
         else if (computerSelection === "paper"){
             console.log("better luck next round!");
+            return false
         }
     }
 
     else if (flatPlayerSelection === "paper") {
         if (computerSelection === "rock"){
             console.log("round goes to player");
+            return true
         }
         else if (computerSelection === "scissors"){
             console.log("better luck next round!");
+            return false
         }
     }
 
     else if (flatPlayerSelection === "scissors") {
         if (computerSelection === "paper"){
             console.log("round goes to player");
+            return true
         }
         else if (computerSelection === "rock"){
             console.log("better luck next round!");
+            return false
         }
     }
 
@@ -68,16 +53,29 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (i=0; i < 5; i++) {
-//         const playerWin = playRound(); 
-//         if (playerWin) playerScore++
-//         else if (!playerWin) computerScore++
-//         else continue
-//     }
-// }
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let computer = "";
+    for (i=0; i < 5; i++) { 
+        computer = getComputerChoice();
+        const playerWin = playRound(playerSelection, computer);
+        if (playerWin === "draw") continue
+        else if (playerWin) playerScore++
+        else if (!playerWin) computerScore++
+        
+    }
+    console.log(`Player: ${playerScore}, Computer ${computerScore}`);
+    if (playerScore > computerScore) {
+        console.log("Well Done!");
+    } 
+    else if (playerScore < computerScore) {
+        console.log("Better luck next time!");
+    }
+    else {
+        console.log("DRAW");
+    }
+}
 
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
